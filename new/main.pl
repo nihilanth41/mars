f02b8c087642d3a04741a05e1bada4490a38c374
Zachary Rump <zrrm74@mail.missouri.edu>
2015-07-09 16:03:46 -0500

    Added main.pl

---
+++ new/main.pl
@@ -0,0 +1,21 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+#use IO::Uncompress::Unzip qw(unzip $UnzipError);
+
+#Check for proper number of command line args (at least 1)
+if($#ARGV < 0)
+{
+	print "Error: not enough arguments\n";
+	printf("Usage is: %s <filename>.zip [config_file]\n", $0);
+}
+
+
+
+
+
+
+exit;
+
+
