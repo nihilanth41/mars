88f88426a70c314528bba8dc1477c6105d00b945
Zachary Rump <zrrm74@mail.missouri.edu>
2015-07-16 11:48:01 -0500

    Introduces working split_nosort(). Still needs gone over but I think it sorts everything properly

--- main.pl
+++ main.pl
@@ -90,37 +90,48 @@ closedir(DIR);
 
 #sort_nosplit($path_to_files, %filename_hash) 
 sub sort_nosplit {
-#my $xls_re = /(.+)[.]xls$/;
 my $path_to_files = $_[0]; 
-if(-d $path_to_files) { print `mv -v $path_to_files/*.xls $path_to_files/$datestamp/XLS/`; }	#Move excel files to their own folder (Can/should we use a regex as hash key for this?) 
+
+#Move excel files to their own folder to get them out of the way 
+if(-d $path_to_files) { print `mv -v $path_to_files/*.xls $path_to_files/$datestamp/XLS/`; }	#my $xls_re = /(.+)[.]xls$/;
+
+#%filename_hash is used to look up the destination folder for a given file, using the same string that we matched the file with. 
 my %filename_hash = ( 
 	MESH => "$datestamp/School/MU_HSL",		#Any filename containing m/mesh/i goes in MU_HSL folder 
 	GENRE => "$datestamp/Genre",   	 		#Any filename containing m/genre/i goes in Genre folder 
-	#'SUBJ.' => "$datestamp/School",			#SUBJ.CHG0/SUBJ.DEL0
-	#'NAME.' => "$datestamp/School",			#NAME.CHG0/NAME.DEL0
-	#'SERIES.' => "$datestamp/School",  		#SERIES.CHG0/SERIES.DEL0
-	#'TITLE.' => "$datestamp/School", 		#TITLE.CHG0/TITLE.DEL0
-	#R03/04/06/07/31 include all the school reports but also some of the misc reports
-	#Can either use the report# as identifier and add rules for MISC files after the fact
-	#or use a more unique identifier in the first place such as SUBJ.CHG0
+	CHILDRENS => "$datestamp/Misc", 		#Any filename containing m/childrens/i goes in Misc 
+	LOCAL => "$datestamp/Misc", 			#Any filename containing m/local/i goes in Misc 
+	R03 => "$datestamp/School",
+	R04 => "$datestamp/School",
+	R06 => "$datestamp/School",
+	R07 => "$datestamp/School", 
+	R31 => "$datestamp/School",
 );
+	
+#@ordered_keys is an ordered list of key strings to match filenames with. 
+#Mesh and Genre are done first because any file with that name goes into a specific folder. 
+#Childrens and local are the two filetypes that do not go with the rest of the reports with the same number (R07, R06) so we deal with those next
+#Then we can move anything with R06/R07 into School, and anything left in the root folder goes in MISC 
+my @ordered_keys = ( "MESH", "GENRE", "CHILDRENS", "LOCAL", "R03", "R04", "R06", "R07", "R31" ); 	
+											
 opendir(my $dh, $path_to_files) || die ("Couldn't open $path_to_files: $!"); 
 #look at each file in the folder 
 while(my $file = readdir($dh)) { 
 next if ($file =~ m/^\./); 	#ignore hidden files 
 next if !($file =~ /\./); 	#ignore files that don't have a . somewhere (used to ignore directories in this case) 
-#look at each key in hash 
-############# CALLING keys() iterates in a random order (but the same order every time as long as the entries haven't changed). ########################
-#Instead we should use a list to store the keys in the proper order (the order we want to check them in) and then use the hash as a lookup table 
-for my $key ( keys %filename_hash )
+
+foreach my $key (@ordered_keys)
 {
 	if( $file =~ m/$key/i )
 	{
-		print `mv -v $path_to_files/$file $path_to_files/$filename_hash{$key}`; 
+		print `mv -v $path_to_files/$file $path_to_files/$filename_hash{$key}`;
+		last; 		#break inner loop when we find the first matching key 
 	}
+} 
 }
-}
-closedir $dh
+closedir $dh;
+#When we get to this point, everything that is left in $path_to_files should go in MISC 
+if(-d $path_to_files) { print `mv -v $path_to_files/*.htm $path_to_files/$datestamp/Misc/`; }
 }
 
 
