74dc387d0be0da3776f1431492562256b36a9cb0
Zachary Rump <zrrm74@mail.missouri.edu>
2015-07-16 14:41:18 -0500

    Adds some information about record percentages to slurp.pl

--- slurp.pl
+++ slurp.pl
@@ -17,7 +17,15 @@ use File::Slurp;
 	}
 }
 
-
+my %NTAR = ( 
+	MU => "41.4",
+	MU_HSL => "2.4",
+	MU_LAW => "2.4",
+	UMKC => "25.7",
+	UMKC_LAW => "3",
+	MST => "6.9",
+	UMSL => "18.2",
+);
 	#attempt to extract records from report 
 	#filename is 1st argument
 	my $filename = $ARGV[0];
@@ -29,7 +37,11 @@ use File::Slurp;
 	my $txt = read_file( $filename ); 
 	my @records = split ( /$search_string/, $txt );
 	my $header = shift @records; #assign the first element of the array to $header, remove it from the array and shift all entries down
+	my $num_records = $#records; #gives the last index of the array, since we removed the header it should be equal to the number of records also
 	my $first_delimiter  = '<td class=\'rec-label\'>(1) Old version of Record:</td>'; #manually create first numbered entry 
+	my $num_records_MU = ($NTAR{"MU"})/100;
+	$num_records_MU *= $num_records;
+	printf("Number of MU records: %d\n", $num_records_MU);
 	my $numbered_rec = join( '', $header,$first_delimiter,$records[0]); #add our header, first numbered entry, and first record to the string  
 	#for each index in array (each record) 
 	for(my $i=1; $i<=$#records; $i++)
