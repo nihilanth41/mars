2a4c1164981b9f65635f7423a9a8d8277f8b66e6
Zachary Rump <zrrm74@mail.missouri.edu>
2015-07-31 16:24:04 -0500

    Adds use utf8::all to fix wide character in print errors

--- parse_csv.pl
+++ parse_csv.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Text::CSV;
+use utf8::all;
 
 
 my $csv = Text::CSV->new ({
@@ -12,8 +13,11 @@ my $csv = Text::CSV->new ({
 	}); 
 
 my $filename = "/home/zrrm74/src/mars/r160.txt";
-open(my $data, '<', $filename) or die "Could not open '$filename' $!\n";
+open(my $data, '<:encoding(utf8)', $filename) or die "Could not open '$filename' $!\n";
 my @controlno = ();
+my @tag = ();
+my @ind = ();
+my @fielddata = ();
 while(my $line = <$data>)
 {
 	chomp $line;
@@ -22,6 +26,9 @@ while(my $line = <$data>)
 	{
 		my @fields = $csv->fields();
 		push @controlno, $fields[0];
+		push @tag, $fields[1];
+		push @ind, $fields[2];
+		push @fielddata, $fields[3];
 	}
 	else
 	{
@@ -31,9 +38,12 @@ while(my $line = <$data>)
 	}
 
 }
-foreach (@controlno)
+$csv->eof or $csv->error_diag();
+close $data;
+
+for(my $i=0; $i<=$#controlno; ++$i)
 {
-	print "\n",$_;
+	print "$i: $controlno[$i], $tag[$i], $ind[$i], $fielddata[$i]\n";
 }
 my $num = $#controlno+1;
 print "No control no: $num\n";
