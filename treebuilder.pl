1522620b70c96d3927a6e42fe4d2941019726de5
Zachary Rump <zrrm74@mail.missouri.edu>
2015-08-06 14:36:19 -0500

    Get ReportExplanation and remove legend node

--- treebuilder.pl
+++ treebuilder.pl
@@ -15,33 +15,55 @@ unless(-e $file)
 my $tree = HTML::TreeBuilder->new();
 $tree->parse_file($file);
 #Do stuff w/ tree here
-my ($HeadingText) = $tree->look_down( 
+my ($HeadingText, $ReportType, $CreatedFor, $CreatedOn, $Count, $ReportExplanation);
+#Get HeadingText 
+($HeadingText) = $tree->look_down( 
 	_tag => "div",
 	id => "HeadingText",
 );
-
-my ($ReportType) = $tree->look_down(
+#Get ReportType
+($ReportType) = $tree->look_down(
 	_tag => "div", 
 	id => "ReportType",
 );
 
+#CreatedFor and CreatedOn have same tag and id, so we capture all matches and assign them manually
 my @CreatedInfo = $tree->look_down(
 	_tag => "div",
 	class => "CreatedInfo",
 );
+$CreatedFor = $CreatedInfo[0];
+$CreatedOn = $CreatedInfo[1];
 
-my $CreatedFor = $CreatedInfo[0];
-my $CreatedOn = $CreatedInfo[1];
+#Get count 
+my @ctl_tag = $tree->look_down(
+	_tag => "td",
+	class => "ctl_no",
+);
+$Count = $#ctl_tag+1;
+
+$ReportExplanation = $tree->look_down(
+	_tag => "div",
+	class => "ReportExplanation",
+);
+
+#Get node of legend 
+my $Legend = $tree->look_down(
+	_tag => "fieldset",
+	class => "legend_set",
+);
+#Remove legend node (child node of ReportExplaination) 
+$Legend->delete;
+
+
 
 print $HeadingText->as_text, "\n";
 print $ReportType->as_text, "\n";
 print $CreatedFor->as_text, "\n";
 print $CreatedOn->as_text, "\n";
+print "Count: $Count\n";
+print $ReportExplanation->as_text, "\n";
 
-my @ctl_tag = $tree->look_down(
-	_tag => "td",
-	class => "ctl_no",
-);
 
 #foreach (@ctl_tag)
 #{
