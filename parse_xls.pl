d1000e4b70a4ba6fd4d6de27425e7c8437d7be67
Zachary Rump <zrrm74@mail.missouri.edu>
2015-08-13 17:10:29 -0500

    Begin parsing xls

--- parse_xls.pl
+++ parse_xls.pl
@@ -2,5 +2,23 @@
 
 use strict;
 use warnings;
+use Spreadsheet::ParseExcel;
 
+my $HOME = $ENV{"HOME"};
+my $file = "$HOME/test.xls";
+
+my $parser = SpreadSheet::ParseExcel->new();
+my $workbook = $parser->parse("$file");
+
+if ( !defined $workbok ) {
+	die $parser->error(), ".\n";
+}
+
+my ($row_min, $row_max) = $worksheet->row_range();
+my ($col_min, $col_max) = $worksheet->col_range();
+
+
+#Construct AoA
+my @rows;
+my @columns;
 
