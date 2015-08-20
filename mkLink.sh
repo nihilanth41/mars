4208739e5b03f88e3befb35702f26137c7ed3112
Zachary Rump <zrrm74@mail.missouri.edu>
2015-08-20 16:56:29 -0500

    Fix mkLink.sh

--- mkLink.sh
+++ mkLink.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-FILES=/usr/local/lso/src/mars/*
+FILE1=/usr/local/lso/src/mars/main.pl
+FILE2=/usr/local/lso/src/mars/treebuilder.pl
 TARGET=/usr/local/lso/bin
-for file in $FILES
-do
-	if [ "$file" == "mkLink.sh" ]; then
-		continue;
-	fi
-	if [ "$file" == "mars.cfg" ]; then
-		continue;
-	fi
-	echo "Processing $file ..."
-	if [ -f $TARGET/$file ]; then
-		echo "$file already exists in $TARGET"
-		read -r -p "Overwrite? [y/N] " yn
-		case $yn in 
-			[Yy]* ) ln -sfn $file $TARGET/$file; continue;;
-			[Nn]* ) continue;;	
-			* ) echo "Please enter yes or no.";;
-		esac
-	fi
-done
+file1=${FILE1##*/}
+file2=${FILE2##*/}
+ln -sfn $FILE1 $TARGET/$file1
+ln -sfn $FILE2 $TARGET/$file2
 
 
